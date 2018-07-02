edfc23ff96feaafc77b468bc9e565f8ee0fa012e
boyarce <b.nelson@alumnos.duoc.cl>
2018-07-02 12:31:58 -0400

    se suben cambios en clientes.php

--- clientes.php
+++ clientes.php
@@ -8,8 +8,8 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-      <!--link rel="stylesheet" href="css/assets/font-awesome/css/font-awesome.min.css"-->
-      <!--link rel="stylesheet" href="css/assets/css/style.css"-->
+      <link rel="stylesheet" href="css/assets/font-awesome/css/font-awesome.min.css">
+      <link rel="stylesheet" href="css/assets/css/style.css">
   </head>
   <body>
     <!-- Navigation -->
@@ -40,7 +40,6 @@
     <!--content-->
     <header class="bg-light text-dark" style="background-image: url('https://image.freepik.com/vector-gratis/fondo-luminoso-con-puntos_1055-3132.jpg')">
       <br/>
-      <br>
       <div class="container">
         <div class="row">
           <div class="col-sm-6">
@@ -80,13 +79,17 @@
 
                   <!-- Modal Header -->
                   <div class="modal-header">
-                    <h4 class="modal-title">Modal Heading</h4>
+                    <h4 class="modal-title">Login</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                   </div>
 
                   <!-- Modal body -->
                   <div class="modal-body">
-                    Modal body..
+                    <div class="col">
+                      <input class="form-control" type="email" name="username" placeholder="Email" required><br/>
+                      <input class="form-control" type="password" name="password" placeholder="Password" required><br/>
+                      <input class="btn btn-primary" type="submit" value="Login">
+                    </div>
                   </div>
 
                   <!-- Modal footer -->
