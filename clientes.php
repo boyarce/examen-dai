7a306465c4577d0329df81ffb608ba5b917773ae
boyarce <b.nelson@alumnos.duoc.cl>
2018-07-02 12:18:42 -0400

    se suben cambios en clientes.php

--- clientes.php
+++ clientes.php
@@ -1,4 +1,4 @@
-<?php  ?>
+<?php  include_once "model/connection/conecctionBD.php"?>
 <html lang="en" dir="ltr">
   <head>
     <title>Clientes</title>
@@ -8,8 +8,8 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-      <link rel="stylesheet" href="css/assets/font-awesome/css/font-awesome.min.css">
-      <link rel="stylesheet" href="css/assets/css/style.css">
+      <!--link rel="stylesheet" href="css/assets/font-awesome/css/font-awesome.min.css"-->
+      <!--link rel="stylesheet" href="css/assets/css/style.css"-->
   </head>
   <body>
     <!-- Navigation -->
@@ -39,34 +39,69 @@
     <br/>
     <!--content-->
     <header class="bg-light text-dark" style="background-image: url('https://image.freepik.com/vector-gratis/fondo-luminoso-con-puntos_1055-3132.jpg')">
+      <br/>
+      <br>
       <div class="container">
-        <h1 class="text-dark">Registro de cliente</h1>
         <div class="row">
           <div class="col-sm-6">
             <img src="https://previews.123rf.com/images/kchung/kchung1411/kchung141100097/33558807-vista-desde-arriba-del-lugar-elementos-de-trabajo-en-el-cuadro-blanco.jpg"
-            with="500">
+            with="500" height="600" scrolling="no" frameborder="no">
           </div>
-          <div class="col-sm-6">
-            <h2>Stacked form</h2>
+          <div class="col-sm-6 text-justify bg-white">
+            <h2>Registro de cliente</h2>
               <form action="/action_page.php">
                 <div class="form-group text-justify">
-                  <label for="email">Email:</label>
-                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                  <input type="run" class="form-control" id="run" placeholder="Run" name="run">
                 </div>
-                <div class="form-group">
-                  <label for="pwd">Password:</label>
-                  <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                <div class="form-group text-justify">
+                  <input type="text" class="form-control" id="nombre" placeholder="Nombre" name="nombre">
+                </div>
+                <div class="form-group text-justify">
+                  <input type="password" class="form-control" id="pwd" placeholder="Password" name="pswd">
+                </div>
+                <div class="form-group text-justify">
+                  <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+                </div>
+                <div class="form-group text-justify">
+                  <input type="text" class="form-control" id="number" placeholder="telefono" name="number">
                 </div>
                 <div class="form-group form-check">
                   <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                    <input class="form-check-input" type="checkbox" name="remember"> Acepto terminos y <a href="#">condiciones</a>
                   </label>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Registar</button>
               </form>
+              <p>ya tienes cuenta? <a class="text-primary" data-toggle="modal" data-target="#myModal">Login</a> aca
+                <!-- The Modal -->
+            <div class="modal" id="myModal">
+              <div class="modal-dialog">
+                <div class="modal-content">
+
+                  <!-- Modal Header -->
+                  <div class="modal-header">
+                    <h4 class="modal-title">Modal Heading</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>
+
+                  <!-- Modal body -->
+                  <div class="modal-body">
+                    Modal body..
+                  </div>
+
+                  <!-- Modal footer -->
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                  </div>
+
+                </div>
+              </div>
+            </div></p>
+
           </div>
         </div>
       </div>
+
     </header>
     <!--Social-media-->
     <section class="py-5">
